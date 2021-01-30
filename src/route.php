5ba29886bc05cfe3cab7794e52f89354823520d1
johnlee <16249682+Johnandfu@users.noreply.github.com>
2021-01-30 15:32:35 +0800

    Update route.php

--- src/route.php
+++ src/route.php
@@ -4,7 +4,7 @@
  *
  * @copyright Copyright (c) 2018 XINRENNET
  */
-
-\think\Route::any('swagger', '\\westhack\\swagger\\IndexController@index');
-\think\Route::any('swagger/api', '\\westhack\\swagger\\ApiController@api');
+use think\facade\Route;
+Route::any('swagger', '\\westhack\\swagger\\IndexController@index');
+Route::any('swagger/api', '\\westhack\\swagger\\ApiController@api');
 
